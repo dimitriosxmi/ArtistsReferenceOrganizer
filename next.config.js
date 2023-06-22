1f42ade9ab3f724ab6dea85726626e03db7bf40f
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-22 20:04:20 +0200

    Setup Next.config.js with @svgr/webpack and throw out useless svg's

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
       // },
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
