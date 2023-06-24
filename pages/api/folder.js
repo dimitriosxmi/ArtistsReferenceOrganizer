3b56fd5e2ec7ebbc48e37cb8da43edae711c3f97
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-24 12:02:26 +0200

    Delete hello.js (uneccessary api). Add folder.js api file (handle POST request)

---
+++ pages/api/folder.js
@@ -0,0 +1,25 @@
+import dbConnect from "../../db/connect.js";
+import Folder from "../../db/models/Folder.js";
+
+const handler = async (request, response) => {
+  await dbConnect();
+
+  if (request.method === "POST") {
+    try {
+      const folder = request.body;
+      await Folder.create(folder);
+
+      response.status(200).json({ status: "Success: Created Folder" });
+      return;
+    } catch (error) {
+      console.log(error);
+
+      response
+        .status(400)
+        .json({ status: "Failed to create folder!", error: error.message });
+      return;
+    }
+  }
+};
+
+export default handler;
