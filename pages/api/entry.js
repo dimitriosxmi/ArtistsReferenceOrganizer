21f7a562cf817bcccd467bb2bbbbcbde3c1dfe73
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-26 12:34:28 +0200

    Add entry.js api page (handles POST request for a single entries)

---
+++ pages/api/entry.js
@@ -0,0 +1,27 @@
+import dbConnect from "../../db/connect.js";
+// Models
+import Entry from "../../db/models/Entry.js";
+
+const handler = async (request, response) => {
+  await dbConnect();
+
+  if (request.method === "POST") {
+    try {
+      const entry = request.body;
+      console.log(entry);
+      await Entry.create(entry);
+
+      response.status(201).json({ status: "Success entry post!" });
+      return;
+    } catch (error) {
+      console.log(error);
+
+      response
+        .status(400)
+        .json({ status: "Failed to post entry!", error: error.message });
+      return;
+    }
+  }
+};
+
+export default handler;
