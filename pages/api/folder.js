e7363063ea3905c590a9744ebdf031d9eaeb6841
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-27 08:25:59 +0200

    Update folder.js api page (Handles GET method with a provided folderId)

--- pages/api/folder.js
+++ pages/api/folder.js
@@ -3,7 +3,26 @@ import dbConnect from "../../db/connect.js";
 import Folder from "../../db/models/Folder.js";
 
 const handler = async (request, response) => {
+  const { folderId } = request.query;
   await dbConnect();
+
+  if (request.method === "GET" && folderId) {
+    try {
+      const folder = await Folder.findById(folderId);
+
+      response
+        .status(200)
+        .json({ status: "Success to get folder!", data: folder });
+      return;
+    } catch (error) {
+      console.log(error);
+
+      response
+        .status(400)
+        .json({ status: "Faillure to get folder!", error: error.message });
+      return;
+    }
+  }
 
   if (request.method === "POST") {
     try {
