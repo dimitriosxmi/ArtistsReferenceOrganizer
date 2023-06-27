ce58ca66f0149db26374395d460e728c515030a9
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-27 16:22:31 +0200

    Update entry.js api page (Handle GET method for entry by id).

--- pages/api/entry.js
+++ pages/api/entry.js
@@ -3,7 +3,24 @@ import dbConnect from "../../db/connect.js";
 import Entry from "../../db/models/Entry.js";
 
 const handler = async (request, response) => {
+  const { entryId } = request.query;
   await dbConnect();
+
+  if (request.method === "GET") {
+    try {
+      const entry = await Entry.findById(entryId);
+
+      response.status(201).json({ status: "Success entry get!", data: entry });
+      return;
+    } catch (error) {
+      console.log(error);
+
+      response
+        .status(400)
+        .json({ status: "Failure to get entry!", error: error.message });
+      return;
+    }
+  }
 
   // POST an entry.
   if (request.method === "POST") {
