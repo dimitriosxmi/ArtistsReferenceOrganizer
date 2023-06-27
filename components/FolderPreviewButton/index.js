701ba5dbbc65694dbb31ce118ee0632f97712d6d
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-27 08:23:50 +0200

    Update FolderPreviewIcon component (rename to FolderPreviewButton, add Link to it, clicking a folder now navigates to a route)

--- components/FolderPreviewButton/index.js
+++ components/FolderPreviewButton/index.js
@@ -1,17 +1,18 @@
 import styled from "styled-components";
+import Link from "next/link";
 // SVGs
 import { FolderIcon } from "../svgs";
 
-const FolderPreviewIcon = ({ data }) => {
+const FolderPreviewButton = ({ data }) => {
   return (
-    <StyledFolder>
+    <StyledFolderLink href={`/folder/${data._id}`}>
       <StyledFolderIcon foldercolor={data.folderColor} />
       <StyledParagraphText>{data.folderName}</StyledParagraphText>
-    </StyledFolder>
+    </StyledFolderLink>
   );
 };
 
-export default FolderPreviewIcon;
+export default FolderPreviewButton;
 
 const StyledFolderIcon = styled(FolderIcon)`
   z-index: 0;
@@ -20,7 +21,8 @@ const StyledFolderIcon = styled(FolderIcon)`
   color: ${({ foldercolor }) => foldercolor};
 `;
 
-const StyledFolder = styled.div`
+const StyledFolderLink = styled(Link)`
+  display: inline-block;
   z-index: 1;
   width: 30%;
   text-decoration: none;
