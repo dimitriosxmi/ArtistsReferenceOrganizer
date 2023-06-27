ddde21814abe4b0613b5524139267a97acf4b437
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-27 08:33:28 +0200

    Update breadcrumb component (Supports the new flexible breadcrumbs provided by the folderIds as folderNames).

--- components/Breadcrumb/index.js
+++ components/Breadcrumb/index.js
@@ -4,11 +4,12 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const Breadcrumb = () => {
-  const [breadcrumbText, setBreadcrumbText] = useState("");
+  const [breadcrumbText, setBreadcrumbText] = useState("Loading..");
 
   //#region Router
   const router = useRouter();
   const routePath = router.asPath;
+  const routerParams = router.query;
   //#endregion
 
   //#region Breadcrumb text setup
@@ -27,12 +28,29 @@ const Breadcrumb = () => {
         case "/new-entry":
           setBreadcrumbText("(NEW ENTRY)");
           break;
+        case `/folder/${routerParams.folderId}`:
+          (async () => {
+            // Get folder by id
+            const response = await fetch(
+              `/api/folder?folderId=${routerParams.folderId}`
+            );
+
+            if (response.ok) {
+              // Refine data
+              const jsonData = await response.json();
+              const cleanData = jsonData.data;
+              const folderName = cleanData.folderName;
+
+              setBreadcrumbText(`Dashboard/ folder/ ${folderName}`);
+            }
+          })();
+          break;
         default:
           setBreadcrumbText("Unknown Page");
           break;
       }
     })(routePath);
-  }, [routePath]);
+  }, [routePath, routerParams]);
   //#endregion
 
   return <StyledBreadcrumb>{breadcrumbText}</StyledBreadcrumb>;
