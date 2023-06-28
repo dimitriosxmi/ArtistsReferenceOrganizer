cd5681133d0223355d43f6dc0254301e1b34dad3
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-06-28 16:13:21 +0200

    Update [entryId].js page (Use DeleteButton component).

--- pages/[entryId].js
+++ pages/[entryId].js
@@ -1,9 +1,15 @@
+// Hooks
+import { useRouter } from "next/router";
 // Components
 import CommonHeaderLayout from "../components/CommonHeaderLayout";
 import BackButton from "../components/BackButton/BackButton.js";
+import DeleteButton from "../components/DeleteButton";
 import EntryDetails from "../components/EntryDetails";
 
 const EntryView = ({ sideBarOpen, setSideBarOpen }) => {
+  const router = useRouter();
+  const { entryId } = router.query;
+
   return (
     <>
       <CommonHeaderLayout
@@ -11,6 +17,10 @@ const EntryView = ({ sideBarOpen, setSideBarOpen }) => {
         setSideBarOpen={setSideBarOpen}
       />
       <BackButton />
+      <DeleteButton
+        text={"Entry"}
+        deleteAPIUrl={`/api/entry?entryId=${entryId}`}
+      />
       <EntryDetails />
     </>
   );
