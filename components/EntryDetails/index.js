d61bc86ed25d31360ef16fb90ed78f8b4911de99
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-07-04 21:05:31 +0200

    Update EntryDetails component (as stated in US 14, added more details to read when clicking into an entry card).

--- components/EntryDetails/index.js
+++ components/EntryDetails/index.js
@@ -8,11 +8,13 @@ const EntryDetails = () => {
   const { entryId } = router.query;
   const [entryData, setEntryData] = useState();
 
+  // GET entry by id from database.
   useEffect(() => {
     (async () => {
       const response = await fetch(`/api/entry?entryId=${entryId}`);
 
       if (response.ok) {
+        // Unpack received data and pass it to entryData state variable.
         const { data } = await response.json();
         setEntryData(data);
       }
@@ -21,9 +23,32 @@ const EntryDetails = () => {
 
   if (!entryData) return <p>Loading..</p>;
 
-  const { entryName } = entryData;
+  const { entryName, entryReferenceLink, entryTags, entryDescription } =
+    entryData;
 
-  return <StyledHeadline>{entryName}</StyledHeadline>;
+  return (
+    <>
+      {/* Name  */}
+      <StyledHeadline>{entryName}</StyledHeadline>
+      {/* Link */}
+      <StyledText bold>Link:</StyledText>
+      <StyledText borderBottom>{entryReferenceLink}</StyledText>
+      {/* Tags */}
+      <StyledTagContainer>
+        <StyledText noMargin bold>
+          Tags:
+        </StyledText>
+        {entryTags.map((tag) => (
+          <StyledTagCard key={tag._id}>{tag.tagName}</StyledTagCard>
+        ))}
+      </StyledTagContainer>
+      {/* Description */}
+      <StyledText borderTop bold>
+        Description:
+      </StyledText>
+      <StyledText>{entryDescription}</StyledText>
+    </>
+  );
 };
 
 export default EntryDetails;
@@ -31,11 +56,43 @@ export default EntryDetails;
 const StyledHeadline = styled.p`
   max-width: 90%;
   margin: 10px 0 0 5%;
-  padding: 5px;
+  padding: 0.5rem;
   font-size: 2rem;
   text-align: center;
   display: block;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+`;
+
+const StyledText = styled.p`
+  max-width: 90%;
+  margin: 0 0 0 5%;
+  padding: 0.5rem;
+  font-size: 1rem;
+  text-align: start;
+  overflow: hidden;
+
+  ${({ bold }) => (bold ? `font-weight: 600;` : null)}
+  ${({ borderTop }) => (borderTop ? `border-top: 2px dashed #223;` : null)}
+  ${({ borderBottom }) =>
+    borderBottom ? `border-bottom: 2px dashed #223;` : null}
+  ${({ noMargin }) => (noMargin ? `margin: 0;` : null)}
+`;
+
+const StyledTagCard = styled.div`
+  margin: 0.125rem 0.25rem;
+  padding: 0 0.25rem;
+  border: 1px solid #223;
+  border-radius: 10px;
+  font-size: 1rem;
+`;
+
+const StyledTagContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  width: 90%;
+  padding-left: 5%;
 `;
