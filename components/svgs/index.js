46261bf109f51d0a3dd16d9ba140b0f49d966305
Dimitrios Kitsikidis <ixbayOriginal@gmail.com>
2023-07-07 15:49:35 +0200

    Add CaretDownIcon svg and CaretRightIcon svg (also export them in the components/svgs/index.js file).

--- components/svgs/index.js
+++ components/svgs/index.js
@@ -6,3 +6,5 @@ export { default as PlusIcon } from "./PlusIcon.svg";
 export { default as EntryFileIcon } from "./EntryFileIcon.svg";
 export { default as PenIcon } from "./PenIcon.svg";
 export { default as ImageIcon } from "./ImageIcon.svg";
+export { default as CaretRightIcon } from "./CaretRightIcon.svg";
+export { default as CaretDownIcon } from "./CaretDownIcon.svg";
